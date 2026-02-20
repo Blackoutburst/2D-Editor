5aacc67948146898019737a538ae01e0a59d262e
Blackoutburst <github@blackoutburst.dev>
2026-02-20 15:28:08 +0100

    texture size per layer

--- src/main/kotlin/dev/blackoutburst/editor/ui/LayerEditPanel.kt
+++ src/main/kotlin/dev/blackoutburst/editor/ui/LayerEditPanel.kt
@@ -11,6 +11,7 @@ import dev.blackoutburst.bogel.utils.Color
 import dev.blackoutburst.bogel.window.Window
 import dev.blackoutburst.editor.inputs.getScreenPosition
 import dev.blackoutburst.editor.tiles.TileLayer
+import dev.blackoutburst.editor.tiles.TilesManager
 
 object LayerEditPanel {
     var layer: TileLayer? = null
@@ -23,12 +24,15 @@ object LayerEditPanel {
 
                 layerGridField.inputText.text = value.gridSize.toString()
                 layerGridField.cursorPosition = value.gridSize.toString().length
+
+                textureSizeField.inputText.text = value.textureSize.toString()
+                textureSizeField.cursorPosition = value.textureSize.toString().length
             }
         }
 
     private const val MARGIN = 10f
     private const val PANEL_WIDTH = 200f
-    private const val PANEL_HEIGHT = 400f
+    private const val PANEL_HEIGHT = 420f
 
     private val background = ColoredBox2D(0f, 0f, PANEL_WIDTH + 10, PANEL_HEIGHT + 10, Color.DARK_GRAY, 10f)
     private val hueRotate = HuePicker(0f, 0f, 180f, 20f, 8f)
@@ -45,6 +49,16 @@ object LayerEditPanel {
 
     private val layerGridText = Text(0f, 0f, 16f, "Grid size")
     private val layerGridField = TextField(
+        x = 0f,
+        y = 0f,
+        width = 180f,
+        height = 25f,
+        borderRadius = 8f,
+        numberOnly = true
+    )
+
+    private val textureSizeText = Text(0f, 0f, 16f, "Texture size")
+    private val textureSizeField = TextField(
         x = 0f,
         y = 0f,
         width = 180f,
@@ -132,6 +146,39 @@ object LayerEditPanel {
             }
         }
 
+        // TEXTURE SIZE
+        elementY -= 30f
+
+        // LAYER GRID
+        textureSizeText.x = elementX
+        textureSizeText.y = elementY
+
+        elementY -= 30f
+
+        textureSizeField.x = elementX
+        textureSizeField.y = elementY
+
+        textureSizeField.onExit {
+            textureSizeField.backgroundColor = Color(0.1f)
+            textureSizeField.outlineColor = Color(0.2f)
+        }
+        textureSizeField.onHover {
+            textureSizeField.backgroundColor = Color(0.2f)
+            textureSizeField.outlineColor = Color(0.3f)
+        }
+        textureSizeField.onClick {}
+
+        textureSizeField.update {
+            try {
+                val size = textureSizeField.inputText.text.toInt()
+                layer!!.textureSize = size
+                TilesManager.refreshDiffuseMap()
+            } catch (_: Exception) {
+                textureSizeField.inputText.text = layer!!.textureSize.toString()
+                textureSizeField.cursorPosition = layer!!.textureSize.toString().length
+            }
+        }
+
         val mp = Mouse.getScreenPosition()
         if (mp.x >= background.x && mp.x <= background.x + PANEL_WIDTH && mp.y <= background.y + PANEL_HEIGHT && mp.y >= background.y) {
             Mouse.update()
@@ -152,5 +199,8 @@ object LayerEditPanel {
 
         layerGridText.render()
         layerGridField.render()
+
+        textureSizeText.render()
+        textureSizeField.render()
     }
 }
